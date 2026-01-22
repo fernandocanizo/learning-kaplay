4ca1b1bf69dd6e78c0899fee04d125f91131f6e8
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2026-01-22 08:36:48 -0300

    Put everything into a "game" scene
    So now we can have different scenes and call them upon events or actions.

--- src/main.js
+++ src/main.js
@@ -8,47 +8,51 @@ kaplay({
 loadRoot("./") // A good idea for Itch.io publishing later
 loadSprite("bean", "sprites/bean.png")
 
-setGravity(1600)
+scene("game", () => {
+  setGravity(1600)
 
-const player = add([sprite("bean"), pos(30, 600), area(), body()])
+  const player = add([sprite("bean"), pos(30, 600), area(), body()])
 
-add([
-  // platform
-  rect(width(), 48),
-  pos(0, height() - 48),
-  outline(4),
-  area(),
-  body({ isStatic: true }),
-  color(127, 200, 255),
-])
-
-const spawnTree = () => {
   add([
-    "tree",
-    rect(48, rand(30, 90)),
+    // platform
+    rect(width(), 48),
+    pos(0, height() - 48),
+    outline(4),
     area(),
     body({ isStatic: true }),
-    outline(4),
-    pos(width(), height() - 48),
-    anchor("botleft"),
-    color(255, 180, 255),
-    move(LEFT, 480),
+    color(127, 200, 255),
   ])
 
-  wait(rand(0.5, 1.5), () => {
-    spawnTree()
-  })
-}
+  const spawnTree = () => {
+    add([
+      "tree",
+      rect(48, rand(30, 90)),
+      area(),
+      body({ isStatic: true }),
+      outline(4),
+      pos(width(), height() - 48),
+      anchor("botleft"),
+      color(255, 180, 255),
+      move(LEFT, 480),
+    ])
 
-spawnTree()
-
-onKeyPress("space", () => {
-  if (player.isGrounded()) {
-    player.jump()
+    wait(rand(0.5, 1.5), () => {
+      spawnTree()
+    })
   }
+
+  spawnTree()
+
+  onKeyPress("space", () => {
+    if (player.isGrounded()) {
+      player.jump()
+    }
+  })
+
+  player.onCollide("tree", () => {
+    addKaboom(player.pos)
+    shake()
+  })
 })
 
-player.onCollide("tree", () => {
-  addKaboom(player.pos)
-  shake()
-})
+go("game")
