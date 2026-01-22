ca1e5ebd5906cba4a54099c140d72ae9f6cb1354
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2026-01-22 07:27:55 -0300

    Reposition Bean, add gravity, add jump key
    Bean falls off the view if you aren't quick with the jump key.
    
    Also, I'm not sure if using Kaplay methods alone or with the object `k`,
    on one side, I'll get tired very quickly that everything starts with a
    "k.". On the other, I get natural completion when pressing "k.<tab>".
    
    I guess I'll leave both and I'll have to get used to it.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import kaplay from "kaplay";
+import kaplay from "kaplay"
 // import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+const k = kaplay()
 
-k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
+loadRoot("./"); // A good idea for Itch.io publishing later
+loadSprite("bean", "sprites/bean.png")
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+k.setGravity(1600)
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+const player = add([sprite("bean"), pos(30, 600), area(), body()])
+
+onClick(() => addKaboom(mousePos()))
+
+onKeyPress("space", () => {
+    player.jump()
+})
